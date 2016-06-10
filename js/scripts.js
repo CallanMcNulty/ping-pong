58cd60fc2712388ab39d23f4808ee5502853cc84
Callan McNulty <jabberwocky222@gmail.com>
2016-06-10 08:51:14 -0700

    Add very basic user interface to html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
-var inputNumber = 16; //test value
-
-var outputArray = [];
-var increment = inputNumber/Math.abs(inputNumber);
-for(var i=increment; i!=inputNumber+increment; i+=increment) {
-  var item = "";
-  if(i%3===0) {
-    item += "ping";
+var pingPong = function(inputNumber) {
+  var outputArray = [];
+  var increment = inputNumber/Math.abs(inputNumber);
+  for(var i=increment; i!=inputNumber+increment; i+=increment) {
+    var item = "";
+    if(i%3===0) {
+      item += "ping";
+    }
+    if(i%5===0) {
+      item += "pong";
+    }
+    if(item==="") {
+      item = i;
+    }
+    outputArray.push(item);
   }
-  if(i%5===0) {
-    item += "pong";
-  }
-  if(item==="") {
-    item = i;
-  }
-  outputArray.push(item);
+  return outputArray;
 }
+
+
+$(document).ready(function() {
+  $("form#main").submit(function(event) {
+    event.preventDefault();
+    $("#out").empty();
+    var input = parseInt($("#in").val());
+    $("#in").val("");
+    var output = pingPong(input);
+    output.forEach(function(outValue) {
+      $("#out").append("<li>"+outValue+"</li>");
+    });
+  });
+});
